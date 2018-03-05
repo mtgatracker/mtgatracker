eb25541db0550afa3e9fd91dd781a13f6ea0bb16
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-03-04 23:57:42 -0800

    Add missing OGW lands (why are these in mtga, lol)

--- set_data/weird.py
+++ set_data/weird.py
@@ -4,8 +4,13 @@ import set_data.rix as rix
 import set_data.xln as xln
 
 CinderBarrens = mcard.Card("cinder_barrens", "Cinder Barrens", [], ["B", "R"], "Land", "", "OGW", -1, 62499)
+TranquilExpanse = mcard.Card("tranquil_expanse", "Tranquil Expanse", [], ["G", "W"], "Land", "", "OGW", -1, 62523)
+MeanderingRiver = mcard.Card("meandering_river", "Meandering River", [], ["B", "w"], "Land", "", "OGW", -1, 62509)
+SubmergedBoneyard = mcard.Card("submerged_boneyard", "Submerged Boneyard", [], ["B", "U"], "Land", "", "OGW", -1, 62519)
+TimberGorge = mcard.Card("timber_gorge", "Timber Gorge", [], ["G", "R"], "Land", "", "OGW", -1, 62521)
 
-WeirdLands = set.Set("rivals_of_ixalan", cards=[CinderBarrens])
+WeirdLands = set.Set("rivals_of_ixalan", cards=[CinderBarrens, TranquilExpanse, MeanderingRiver, TimberGorge,
+                                                SubmergedBoneyard])
 
 BasicLands = set.Pool("all_basic_lands", cards=[
     rix.Plains, xln.Plains, xln.Plains2, xln.Plains3, xln.Plains4,
