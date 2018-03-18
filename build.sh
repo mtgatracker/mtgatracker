2dfacd1e223c83e0ef2c2c31885a9959012a4a32
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-03-18 16:21:15 -0700

    add sitemap

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-pyinstaller.exe app/mtgatracker_backend.py --distpath appdist
-rm -rf build
-rm -rf mtgatracker_backend.spec
+yes | pyinstaller.exe app/mtgatracker_backend.py --distpath appdist
+#rm -rf build
+#rm -rf mtgatracker_backend.spec
 
-./node_modules/.bin/electron-packager electron/ MTGATracker --overwrite --electron-version=1.7.6 --version=0.0.0 --extra-resource="appdist" --icon="electron/img/icon.ico"
+yes | ./node_modules/.bin/electron-packager electron/ MTGATracker \
+  --overwrite --version=0.0.0 --electron-version=1.7.6 \
+  --ignore="\.git.*" --ignore=".*psd" --ignore="mtga_watch\.log.*" \
+  --extra-resource="appdist" \
+  --icon="electron/img/icon.ico" \
+  --asar
