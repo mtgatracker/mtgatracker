6dc775066b3d0ea8c1d7b74f949dae0171f30b36
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-03-22 15:11:39 -0700

    Add setdata from new sets (closes #60)

--- scripts/generate_base_mtga_id_lookup.py
+++ scripts/generate_base_mtga_id_lookup.py
@@ -3,6 +3,13 @@ import re
 
 default_delta = 2  # card ID goes up by 2, to account for... foils, I guess?
 
+# 64813 = cartouche of solidarity
+# 65273 = Spring /// Mind
+# 65729 = pride sovereign
+# 65349 = start /// finish
+
+special_layouts = {"aftermath": 3}
+
 
 def add_cardIDs_to_cardset(cardset, start_id):
     for card in cardset["cards"]:
@@ -14,15 +21,28 @@ def add_cardIDs_to_cardset(cardset, start_id):
             card["number_ab"] = a_b
         card["number_int"] = int(card_number)
         card["mtga_id"] = int(start_id)
-        start_id += 2
+        if "layout" in card.keys() and card["layout"] in special_layouts.keys():
+            start_id += special_layouts[card["layout"]]
+        else:
+            start_id += 2
 
 
-with open("../set_data/XLN.json", "r", encoding='utf-8') as xln_r:
+with open("../app/set_data/XLN.json", "r", encoding='utf-8') as xln_r:
     ixalan_card_set = json.load(xln_r)
     add_cardIDs_to_cardset(ixalan_card_set, 65961)  # 65961 = adanto's vanguard, xln card 1
-with open("../set_data/RIX.json", "r", encoding='utf-8') as rix_r:
+
+with open("../app/set_data/RIX.json", "r", encoding='utf-8') as rix_r:
     rivals_card_set = json.load(rix_r)
     add_cardIDs_to_cardset(rivals_card_set, 66619)  # 66619 = baffling end, rix card 1
+
+with open("../app/set_data/AKH.json", "r", encoding='utf-8') as akh_r:
+    amonkhet_card_set = json.load(akh_r)
+    add_cardIDs_to_cardset(amonkhet_card_set, 64801)  # 64801 = angel of sanctions, akh card 1
+
+with open("../app/set_data/HOU.json", "r", encoding='utf-8') as hou_r:
+    hour_card_set = json.load(hou_r)
+    add_cardIDs_to_cardset(hour_card_set, 65479)  # 65479 = act of heroism, hou card 1
+
 
 lookup = {}
 for card in rivals_card_set["cards"]:
@@ -31,92 +51,65 @@ for card in rivals_card_set["cards"]:
 for card in ixalan_card_set["cards"]:
     lookup[card["mtga_id"]] = card
 
-with open("xln.py", "w") as ixw:
-    header = """
+
+def write_set(set_mnemonic, set_fullname, card_data):
+    set_name_class_cased = re.sub('[^0-9a-zA-Z_]', '', set_fullname)
+    set_name_snake_cased = re.sub('[^0-9a-zA-Z_]', '', set_fullname.lower().replace(" ", "_"))
+    with open("{}.py".format(set_mnemonic.lower()), "w") as set_file:
+        header = """
 import sys
-from models.card import Card
-from models.set import Set
+from app.models.card import Card
+from app.models.set import Set
 import inspect
 """
 
-    footer = """
+        footer = """
 clsmembers = [card for name, card in inspect.getmembers(sys.modules[__name__]) if isinstance(card, Card)]
-Ixalan = Set("ixalan", cards=clsmembers)
-"""
-    already_written = set()
-    ixw.write("{}\n\n".format(header))
-    for card in ixalan_card_set["cards"]:
-        card_name_class_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"])
-        card_name_class_cased_suffixed = card_name_class_cased
-        card_suffix = 2
-        while card_name_class_cased_suffixed in already_written:
-            card_name_class_cased_suffixed = card_name_class_cased + str(card_suffix)
-            card_suffix += 1
-        card_name_snake_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"].lower().replace(" ", "_"))
-        mana_cost, color_identity = [], []
-        if "manaCost" in card.keys():
-            mana_cost = [m for m in re.split("[{}]", card["manaCost"]) if m]
-        if "colorIdentity" in card.keys():
-            color_identity = card["colorIdentity"]
-        card_type = card["type"].replace(" — ", "-")
-        subtype = ""
-        if "-" in card_type:
-            card_type, subtype = card_type.split("-")
-        ixw.write('{} = Card("{}", "{}", {}, {}, "{}", "{}", "XLN", {}, {})\n'.format(
-            card_name_class_cased_suffixed,
-            card_name_snake_cased,
-            card["name"],
-            mana_cost,
-            color_identity,
-            card_type,
-            subtype,
-            card["number_int"],
-            card["mtga_id"])
-        )
-        already_written.add(card_name_class_cased_suffixed)
-    ixw.write("{}\n".format(footer))
+{} = Set("{}", cards=clsmembers)
+""".format(set_name_class_cased, set_name_snake_cased)
+        already_written = set()
+        set_file.write("{}\n\n".format(header))
+        for card in card_data["cards"]:
+            card_name_class_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"])
+            card_name_class_cased_suffixed = card_name_class_cased
+            card_suffix = 2
 
-with open("rix.py", "w") as ixw:
-    header = """
-import sys
-from models.card import Card
-from models.set import Set
-import inspect
-"""
+            while card_name_class_cased_suffixed in already_written:
+                card_name_class_cased_suffixed = card_name_class_cased + str(card_suffix)
+                card_suffix += 1
 
-    footer = """
-clsmembers = [card for name, card in inspect.getmembers(sys.modules[__name__]) if isinstance(card, Card)]
-RivalsOfIxalan = Set("rivals_of_ixalan", cards=clsmembers)
-"""
-    already_written = set()
-    ixw.write("{}\n\n".format(header))
-    for card in rivals_card_set["cards"]:
-        card_name_class_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"])
-        card_name_class_cased_suffixed = card_name_class_cased
-        card_suffix = 2
-        while card_name_class_cased_suffixed in already_written:
-            card_name_class_cased_suffixed = card_name_class_cased + str(card_suffix)
-            card_suffix += 1
-        card_name_snake_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"].lower().replace(" ", "_"))
-        mana_cost, color_identity = [], []
-        if "manaCost" in card.keys():
-            mana_cost = [m for m in re.split("[{}]", card["manaCost"]) if m]
-        if "colorIdentity" in card.keys():
-            color_identity = card["colorIdentity"]
-        card_type = card["type"].replace(" — ", "-")
-        subtype = ""
-        if "-" in card_type:
-            card_type, subtype = card_type.split("-")
-        ixw.write('{} = Card("{}", "{}", {}, {}, "{}", "{}", "RIX", {}, {})\n'.format(
-            card_name_class_cased_suffixed,
-            card_name_snake_cased,
-            card["name"],
-            mana_cost,
-            color_identity,
-            card_type,
-            subtype,
-            card["number_int"],
-            card["mtga_id"])
-        )
-        already_written.add(card_name_class_cased_suffixed)
-    ixw.write("{}\n".format(footer))
+            card_name_snake_cased = re.sub('[^0-9a-zA-Z_]', '', card["name"].lower().replace(" ", "_"))
+            mana_cost, color_identity = [], []
+
+            if "manaCost" in card.keys():
+                mana_cost = [m for m in re.split("[{}]", card["manaCost"]) if m]
+
+            if "colorIdentity" in card.keys():
+                color_identity = card["colorIdentity"]
+
+            card_type = card["type"].replace(" — ", "-")
+            subtype = ""
+
+            if "-" in card_type:
+                card_type, subtype = card_type.split("-")
+
+            set_file.write('{} = Card("{}", "{}", {}, {}, "{}", "{}", "{}", {}, {})\n'.format(
+                card_name_class_cased_suffixed,
+                card_name_snake_cased,
+                card["name"],
+                mana_cost,
+                color_identity,
+                card_type,
+                subtype,
+                set_mnemonic,
+                card["number_int"],
+                card["mtga_id"])
+            )
+            already_written.add(card_name_class_cased_suffixed)
+        set_file.write("{}\n".format(footer))
+
+
+write_set("XLN", "Ixalan", ixalan_card_set)
+write_set("RIX", "Rivals Of Ixalan", rivals_card_set)
+write_set("AKH", "Amonkhet", amonkhet_card_set)
+write_set("HOU", "Hour Of Devastation", hour_card_set)
