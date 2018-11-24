65024539bc07a629b3e17255f7387bbc7172636b
Shawn Kiewel <smkiewel@gmail.com>
2018-11-23 22:33:48 -0500

    Generalize merge and sort and fix dupe issue (#431)
    * Fix issues with duplicate cards
    
    Avoid mutating list when adding counts together and don't assume lists are pre-sorted.
    
    * DRY merge and sort code; closes #429; closes #430

--- electron/mainRenderer.js
+++ electron/mainRenderer.js
@@ -292,7 +292,10 @@ let deckFrequencySubLists = function (decklist) {
   let card_count = -1;
   let sublists = [];
   let current_sublist = null;
-  for ( card of decklist ) {
+  let reverse_sort = function(a, b){return b.count_in_deck - a.count_in_deck}
+  // Don't mutate input list
+  sorted_deck = [...decklist].sort(reverse_sort)
+  for ( card of sorted_deck ) {
     if (card.count_in_deck != card_count){
       if ( current_sublist != null ){
         sublists.push(current_sublist);
@@ -305,23 +308,6 @@ let deckFrequencySubLists = function (decklist) {
   sublists.push(current_sublist);
   return sublists;
 };
-
-let sortDecklist = function (decklist) {
-    if (decklist.length === 0) {
-        return decklist;
-    }
-    if (sortMethod.startsWith("draw")) {
-        let subsort = sortMethod.split('-')[1];
-        if ( subsort === undefined ){
-          subsort = 'name';
-        }
-        return drawSort(decklist,subsort);
-    } else if (sortMethod == "emerald") {
-        return emeraldSort(decklist);
-    } else if (sortMethod == "color") {
-        return colorSort(decklist);
-    }
-}
 
 let emeraldSort = function (decklist) {
    return decklist.sort(
@@ -561,38 +547,38 @@ let getColorValue = function(card) {
   return value;
 }
 
-rivets.formatters.drawStatsSort = function(decklist) {
-    return sortDecklist(decklist);
-};
-
-rivets.formatters.drawStatsMergeDuplicates = function(decklist) {
+let mergeDuplicates = function(decklist) {
+    let field = 'pretty_name'
+    if (decklist[0].card != undefined){ field = 'card'}
     let mergedDecklist = new Map();
     decklist.forEach((card) => {
-        if (mergedDecklist.get(card.card)) {
-            mergedDecklist.get(card.card).count_in_deck += card.count_in_deck;
+        if (mergedDecklist.get(card[field])) {
+            mergedDecklist.get(card[field]).count_in_deck += card.count_in_deck;
         }
         else {
-            mergedDecklist.set(card.card, card);
+            mergedDecklist.set(card[field], Object.assign({}, card));
         }
     });
     return Array.from(mergedDecklist.values());
 };
 
 rivets.formatters.decklistSort = function(decklist) {
-    return sortDecklist(decklist);
-};
+    if (decklist.length === 0) {
+        return decklist;
+    }
 
-rivets.formatters.decklistMergeDuplicates = function(decklist) {
-    let mergedDecklist = new Map();
-    decklist.forEach((card) => {
-        if (mergedDecklist.get(card.pretty_name)) {
-            mergedDecklist.get(card.pretty_name).count_in_deck += card.count_in_deck;
+    let sorted_decklist = mergeDuplicates(decklist)
+    if (sortMethod.startsWith("draw")) {
+        let subsort = sortMethod.split('-')[1];
+        if ( subsort === undefined ){
+          subsort = 'name';
         }
-        else {
-            mergedDecklist.set(card.pretty_name, card);
-        }
-    });
-    return Array.from(mergedDecklist.values());
+        return drawSort(sorted_decklist,subsort);
+    } else if (sortMethod == "emerald") {
+        return emeraldSort(sorted_decklist);
+    } else if (sortMethod == "color") {
+        return colorSort(sorted_decklist);
+    }
 };
 
 rivets.binders.showmessage = function(el, value) {
